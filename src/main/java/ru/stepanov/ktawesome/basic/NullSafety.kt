d938fab608141f4e9f2de9caf3dc34128d38982c
petr <fromuralwithlove@gmail.com>
2020-08-03 18:47:31 +1000

    minor

---
+++ src/main/java/ru/stepanov/ktawesome/basic/NullSafety.kt
@@ -0,0 +1,27 @@
+//package ru.stepanov.ktawesome.basic
+//
+//fun main() {
+//    var neverNull: String = "This can't be null"
+//    neverNull = null
+//
+//    var nullable: String? = "You can keep a null here"
+//    nullable = null
+//
+//    var inferredNonNull = "The compiler assumes non-null"
+//    inferredNonNull = null
+//
+//    strLength(neverNull)
+//    strLength(nullable)
+//    strLengthNullable(nullable)
+//}
+//
+//fun strLength(notNull: String): Int {
+//    return notNull.length
+//}
+//
+//fun strLengthNullable(nullable: String?): Int {
+//    //compilator required explicit check
+//    if (nullable != null) {
+//        return nullable.length
+//    }
+//}

cc0ed134fedd370a12f831017ce1d444d43bd7bb
petr <fromuralwithlove@gmail.com>
2020-08-03 17:50:14 +1000

    basic

--- src/main/java/ru/stepanov/ktawesome/basic/Basic.kt
+++ src/main/java/ru/stepanov/ktawesome/basic/Basic.kt
@@ -1,5 +1,29 @@
 package ru.stepanov.ktawesome.basic
 
+fun printMessage(message: String): Unit {
+    println(message)
+}
+
+fun printMessageWithPrefix(message: String, prefix: String = "Info") {
+    println("[$prefix] $message")
+}
+
+fun sum(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun multiply(x: Int, y: Int) = x * y
+
 fun main() {
-    println("Hello World!")
+    printMessage("Hello")
+    printMessageWithPrefix("Hello", "Log")
+    printMessageWithPrefix("Hello")
+    printMessageWithPrefix("Hello")
+    printMessageWithPrefix(prefix = "Log", message = "Hello")
+
+    println(sum(1, 2))
+    println("fun multiply(x: Int, y: Int) = x * y    => " + multiply(1, 2))
+
+    System.out.println("Plain old Java code")
+    println("Call JAVA-code from kotlin: " + JavaUtils.reverse("54321"))
 }
---
+++ src/main/java/ru/stepanov/ktawesome/basic/JavaUtils.java
@@ -0,0 +1,9 @@
+package ru.stepanov.ktawesome.basic;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class JavaUtils {
+    public static String reverse(String s) {
+        return StringUtils.reverse(s);
+    }
+}

20df63d26ae9459fba10905f372f9c36fe8bac80
petr <fromuralwithlove@gmail.com>
2020-08-03 19:11:10 +1000

    class and generics

---
+++ src/main/java/ru/stepanov/ktawesome/basic/7_Class.kt
@@ -0,0 +1,28 @@
+package ru.stepanov.ktawesome.basic
+
+class Customer
+
+data class Contact(
+        val id: Int,
+        var email: String) {
+    fun getSomething(): String {
+        return "returning_string"
+    }
+
+    constructor(id: Int) : this(id, "default@mail.com")
+}
+
+fun main() {
+    val customer = Customer()
+
+    val contact = Contact(1, "mary@gmail.com")
+    val contact_2 = Contact(1)
+
+    println(contact)
+    println(contact_2)
+
+    println(contact.id)
+    println("Change email because VAR:")
+    contact.email = "jane@gmail.com"
+    println("                         " + contact)
+}
